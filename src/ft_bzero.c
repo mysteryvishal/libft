5905f275f7a495a7d61b30ae72592356e3768b7a
Vishal Mistry <vmistry@c1r7s2.42london.com>
2025-10-24 15:19:39 +0100

    done bzero

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,7 +6,35 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 05:42:36 by vmistry           #+#    #+#             */
-/*   Updated: 2025/10/24 05:42:37 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/10/24 15:18:38 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// bzero:
+// 	fills the first n bytes of the object pointed to by dst with zero (NUL) bytes.
+
+#include <stddef.h>
+void    ft_bzero(void *dst, size_t n)
+{
+    size_t  i;
+    unsigned char   *dest;
+
+    dest = (unsigned char *) dst;
+    i = 0;
+    while (i < n)
+        dest[i++] = 0;
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void)
+{
+    char    buffer1[80];
+    char    buffer2[80];
+
+    bzero(buffer1, 80);
+    ft_bzero(buffer2, 80);
+    printf("Equal (0 if equal): %d\n", memcmp(buffer1, buffer2, 80));
+    return (0);
+}*/
