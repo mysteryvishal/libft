2a349d56a72e0c3a33bb019b9db79f0128991328
Vishal Mistry <vmistry@c1r7s2.42london.com>
2025-10-24 16:44:41 +0100

    done memmove

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 12:21:46 by vmistry           #+#    #+#             */
-/*   Updated: 2025/10/24 16:01:16 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/10/24 16:44:20 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_strlen(char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void    ft_bzero(void *dst, size_t n);
 void	*ft_memcpy(void *dest_str, const void *src_str, size_t n);
-// memmove
+void	*ft_memmove(void *dest_str, const void *src_str, size_t numBytes);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	ft_toupper(char c);
