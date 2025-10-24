b33a7dd9d9763d6be135bafcaa79ad90b51c6b4d
Vishal Mistry <vmistry@student.42london.com>
2025-10-24 04:49:48 +0100

    done isalpha

--- src/ft_isalpha.c
+++ src/ft_isalpha.c
@@ -6,16 +6,25 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 13:10:33 by vmistry           #+#    #+#             */
-/*   Updated: 2025/10/22 16:01:58 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/10/23 18:42:50 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(char c)
+int	ft_isalpha(int arg)
 {
-	if ((c >= 'A' && c <= 'Z')
-		|| (c >= 'a' && c <= 'z')
-		|| (c >= '0' && c <= '9'))
+	if ((arg >= 65 && arg <= 90)
+		|| (arg >= 97 && arg <= 122))
 		return (1);
-	else
-		return (0);
+	return (0);
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("Input: %d // Output: %d\n", 65, ft_isalpha(65));
+	printf("Input: %d // Output: %d\n", 120, ft_isalpha(120));
+	printf("Input: %d // Output: %d\n", 0, ft_isalpha(0));
+	printf("Input: %d // Output: %d\n", 2000, ft_isalpha(2000));
+	printf("Input: %c // Output: %d\n", 'A', ft_isalpha('A'));
+	return (0);
+}*/
