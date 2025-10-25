916ddcb3d5ea46457a6b118589b5120defb24edb
Vishal Mistry <vmistry@student.42london.com>
2025-10-25 09:30:20 +0100

    done strrchr

--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 12:21:46 by vmistry           #+#    #+#             */
-/*   Updated: 2025/10/25 00:29:51 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/10/25 09:25:16 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+#include <stddef.h>
 
 int	ft_isalpha(int arg);
 int	ft_isdigit(int arg);
@@ -28,7 +29,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	ft_toupper(char c);
 char	ft_tolower(char c);
 char    *ft_strchr(const char *str, int search_str);
-// strrchr
+char	*ft_strrchr(const char *str, int c);
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
 // memchr
 int	ft_memcmp(const void *str1, const void *str2, size_t n);
