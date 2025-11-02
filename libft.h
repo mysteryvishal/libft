26519c05b4872fe54c8efabd110bf34141e4dfab
Vishal Mistry <vishalmistry51@gmail.com>
2025-11-02 02:06:50 +0000

    feat: add ft_itoa and updated header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 12:21:46 by vmistry           #+#    #+#             */
-/*   Updated: 2025/10/30 16:07:21 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/11/02 02:06:16 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-
+char	*ft_itoa(int n);
 
 #endif
