75321edb9498da09e7694fefdbd0d5ffb5b799d6
Vishal Mistry <vishalmistry51@gmail.com>
2025-11-03 00:11:22 +0000

    feat: add ft_strmapi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 12:21:46 by vmistry           #+#    #+#             */
-/*   Updated: 2025/11/02 02:06:16 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/11/03 00:11:05 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
