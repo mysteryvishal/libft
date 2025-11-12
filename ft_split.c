ef7c7586a23e4be99c618a64ffdce35ab3682c68
Vishal Mistry <vmistry@student.42london.com>
2025-11-12 11:42:58 +0000

    fix: ft_split cleaner error handling and memory management

--- ft_split.c
+++ ft_split.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/29 22:40:25 by vmistry           #+#    #+#             */
-/*   Updated: 2025/11/12 09:34:21 by vmistry          ###   ########.fr       */
+/*   Created: 2025/11/12 10:44:31 by vmistry           #+#    #+#             */
+/*   Updated: 2025/11/12 11:39:14 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@
 
 #include "libft.h"
 
-// numsub = number of substrings
-static size_t	ft_numsub(const char *s, char c)
+static size_t	ft_wordcount(const char *s, char c)
 {
 	size_t	count;
 	int		in_sub;
@@ -40,21 +39,6 @@ static size_t	ft_numsub(const char *s, char c)
 	return (count);
 }
 
-// allocate and copy substring to result array
-static char	*ft_alloc_copy(const char *s, size_t start, size_t end)
-{
-	char	*res;
-
-	if (!s || start >= end)
-		return (NULL);
-	res = malloc(sizeof(char) * (end - start + 1));
-	if (!res)
-		return (NULL);
-	ft_memcpy(res, s + start, end - start);
-	res[end - start] = '\0';
-	return (res);
-}
-
 static void	ft_freeall(char **res, size_t n)
 {
 	size_t	i;
@@ -70,16 +54,34 @@ static void	ft_freeall(char **res, size_t n)
 	free(res);
 }
 
+static int	ft_alloc_copy(char **res, size_t i, char const *s, size_t end)
+{
+	char	*substr;
+
+	if (!s || end <= 0)
+		return (1);
+	substr = malloc(sizeof(char) * (end + 1));
+	if (!substr)
+	{
+		ft_freeall(res, i);
+		return (1);
+	}
+	ft_memcpy(substr, s, end);
+	substr[end] = '\0';
+	res[i] = substr;
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	size_t	end;
+	char	**res;
 	size_t	nsub;
 	size_t	i;
-	char	**res;
+	size_t	end;
 
 	if (!s)
 		return (NULL);
-	nsub = ft_numsub(s, c);
+	nsub = ft_wordcount(s, c);
 	res = malloc(sizeof(char *) * (nsub + 1));
 	if (!res)
 		return (NULL);
@@ -91,33 +93,30 @@ char	**ft_split(char const *s, char c)
 		end = 0;
 		while (s && s[end] && s[end] != c)
 			end++;
-		res[i] = ft_alloc_copy(s, 0, end);
-		if (!res[i])
-		{
-			ft_freeall(res, i);
+		if (ft_alloc_copy(res, i, s, end))
 			return (NULL);
-		}
 		s += end;
 		i++;
 	}
 	res[nsub] = NULL;
 	return (res);
 }
+
 /*
 #include <stdio.h>
 
 int	main(void)
 {
-	char	*str = "Hello;World!;42;;";
-	char	c = ';';
-	char	**res = ft_split(str, c);
-	int	num_substrings = ft_numsub(str, c) + 1;
-	
-	for (int i = 0; i < num_substrings; i++)
+	char	str[] = "Hello;my;name;is";
+	char	del = ';';
+	printf("str: %s\ndel: %c\n", str, del);
+	char **res = ft_split(str, del);
+	if (res == NULL)
 	{
-		printf("res[%d]: %s\n", i, res[i]);
-		free(res[i]);
+		printf("failed: %s\n", *res);
+		return (0);
 	}
-	free(res);
+	for (size_t i = 0; i < ft_wordcount(str, del); i++)
+		printf("out_%ld: %s\n", i, res[i]);
 	return (0);
 }*/
