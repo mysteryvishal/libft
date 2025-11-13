8ac2056660ca4b0ba151df4a912de7533322d9c2
Vishal Mistry <vishalmistry51@gmail.com>
2025-11-13 03:51:56 +0000

    fix: improve ft_memmove null checks and simplify logic

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 05:43:06 by vmistry           #+#    #+#             */
-/*   Updated: 2025/11/13 03:05:37 by vmistry          ###   ########.fr       */
+/*   Updated: 2025/11/13 03:50:35 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,16 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	const unsigned char	*s;
 	unsigned char		*d;
 
-	if (dst == src || len == 0)
-		return (dst);
+	if (!dst || !src)
+		return (NULL);
 	d = (unsigned char *)dst;
 	s = (const unsigned char *)src;
-	if (d < s)
+	if (d > s)
+		while (len-- > 0)
+			d[len] = s[len];
+	else
 		while (len--)
 			*d++ = *s++;
-	else
-	{
-		d += len - 1;
-		s += len - 1;
-		while (len--)
-			*d-- = *s--;
-	}
 	return (dst);
 }
 
